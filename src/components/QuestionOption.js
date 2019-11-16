cb070ee1d967bd1ee5d681137928c34671b9f421
gboyega <jaiyeolagboyega@gmail.com>
2019-11-16 13:31:48 +0100

    finished quiz app

--- src/components/QuestionOption.js
+++ src/components/QuestionOption.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { Checkbox } from 'semantic-ui-react';
-import { Radio } from 'semantic-ui-react';
-import { Header } from 'semantic-ui-react';
-import { Card } from 'semantic-ui-react'
+import { Header, Card } from 'semantic-ui-react';
 import "./styles.css";
 
 function QuestionOption(props){
